7264c1887d35b2aca9a35e53eecf8a2e3fff3907
LJWhappy <ljw2797661571@163.com>
2019-10-16 19:11:45 +0800

    monday 任务

---
+++ src/com/acat/lijingwen/insersort.java
@@ -0,0 +1,89 @@
+package com.acat.lijingwen;
+
+import javax.swing.*;
+
+public class insersort {
+//直接插入排序
+    public static void insert(int R[], int n){
+      int i,j;
+      int temp;
+      for (i=1;i<n;i++){
+          temp=R[i];//将带插入的值放到temp
+          j=i-1;
+          while(j>=0&&temp<R[j]){   //带插入的值小于目前的最大值  则将目前的最大值后移然后将待排的值放到前面合适的地方
+              R[j+1]=R[j];
+              --j;
+          }
+          //这里的j+1排好序的某个位置
+          R[j+1]=temp;
+      }
+      for (int k=0;k<n;k++){
+          System.out.println(R[k]);
+      }
+    }
+    //折半插入排序**
+    private static void binaryInsertSort(int arr[],int length){
+        int low,high,m,temp,i,j;
+        for(i = 1;i<arr.length;i++){
+            //折半查找应该插入的位置
+            low = 0;
+            high = i-1;
+            while(low <= high){
+                m = (low+high)/2;
+                if(arr[m] > arr[i])
+                    high = m - 1;
+                else
+                    low = m + 1;
+            }
+            //统一移动元素，然后将这个元素插入到正确的位置
+            temp = arr[i];
+            for(j=i;j>high+1;j--){
+                arr[j] = arr[j-1];
+            }
+            arr[high+1] = temp;
+        }
+        for (int k=0;k<length;k++){
+            System.out.println(arr[k]);
+         }
+}
+//剑指offer二维数组
+public static boolean Find(int target, int [][] array) {
+    int hang = array.length;
+    int lie = array[0].length;
+    boolean result=false;
+    for (int i=0;i<hang;i++){
+        for (int j=0;j<lie;j++){
+            if (target==array[i][j]){
+                result=true;
+                System.out.println("数组中含有此整数");
+                break;
+            }
+        }
+        if(result)
+            break;
+    }
+    return result;
+}
+    //剑指offer字符串的替换
+    public static String replaceSpace() {
+
+        String str ="acb*db de";
+        return  str.toString().replaceAll(" " , "%20");
+    }
+    public static void main(String []args){
+       /* int [] R={9,5,6,8,7,4,1,3,2};
+        int Ilength=R.length;
+        insersort.insert(R,Ilength);
+        int [] H ={1,2,3,5,4,6};
+        int Hlength=H.length;
+        int key=5;
+        insersort.binaryInsertSort(H,Hlength);*/
+/*        int Array[][]={{1,2},{3,4}};
+        int target=3;
+        boolean reault=insersort.Find(target,Array);
+        System.out.println("reault:"+reault);*/
+       // System.out.println(length);
+        String string=insersort.replaceSpace();
+        System.out.println(string);
+    }
+}
---
+++ src/com/acat/lijingwen/set_1016.java
@@ -0,0 +1,36 @@
+package com.acat.lijingwen;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+public class set_1016 {
+//三个set的比较
+//    public static void main (String []args){
+//        HashSet<String>hashset = new HashSet<>();
+//        LinkedHashSet<String>linkedHashSet = new LinkedHashSet<>();
+//        TreeSet<String> treeSet = new TreeSet<>();
+////        asList 方法的参数必须是对象或者对象数组  "B","E","D","C","A"
+//        for(String data : Arrays.asList("a","b","d","c")){
+//         hashset.add(data);
+//         linkedHashSet.add(data);
+//         treeSet.add(data);
+//        }
+//        System.out.println("hashset:"+hashset);  //有序指的是存取的顺序一致  hashset不保证有序，结果虽然可能有序但是不可依赖
+//        System.out.println("linkedhashset"+linkedHashSet);  //先进先出按插入顺序排序
+//        System.out.println("treeset"+treeSet); //treeset内部进行了排序
+//    }
+
+    //treeset的排序比较
+    public static void main(String []args){
+        //自然顺序的排序
+        TreeSet<Integer> ts =new TreeSet<>();
+        ts.add(20);
+        ts.add(22);
+        ts.add(21);
+        for (int i : ts){
+            System.out.println(i);
+        }
+    }
+}
