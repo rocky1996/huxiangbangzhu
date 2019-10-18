b94f0dc225feae0a39731ba5332d04a078f5c73f
LJWhappy <ljw2797661571@163.com>
2019-10-18 21:38:50 +0800

    wednesday  task

---
+++ src/com/acat/lijingwen/wednesday/BuildThread.java
@@ -0,0 +1,127 @@
+package com.acat.lijingwen.wednesday;
+
+/*
+public class BuildThread extends Thread{
+    public void run(){
+        for (int i = 0;i<10;i++){
+            System.out.println("gongzuo");
+        }
+    }
+    public static void  main (String []args){
+        BuildThread thread = new BuildThread();
+        thread.start();
+        for (int i =0;i<3;i++){
+        System.out.println("changge");
+    }
+  }
+}
+*/
+/*
+* 总结一下：上述代码只是创建了一个线程，如果要创建多个，通过
+* 继承Thread就需要创建多个不同的继承Thread的类.
+* 这里通过建立一个主线程就是main函数的for循环，以及之间创建的线程表示两个线程的运行
+*  */
+
+//实现runnable
+/*public class BuildThread implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i=0;i<10;i++){
+            System.out.println("hahaha");
+        }
+    }
+    public static void main (String [] args){
+        //创建实现类对象
+        BuildThread buildThread = new BuildThread();
+        //创建代理类对象
+        Thread t =new Thread(buildThread);
+        t.start();
+        //一个对象只用一次可以匿名
+// 上面三步可用这一句代替 new Thread(new BuildThread()).start();
+        for (int i=0;i<10;i++){
+            System.out.println("changge");
+        }
+    }
+}*/
+/*
+java 可以实现多个接口  这种方式常用
+*/
+/*public class BuildThread implements Runnable{
+    //票数
+    private int ticketNum=5;
+    @Override
+    public void run() {
+        while (true){
+            if(ticketNum<0)
+                break;
+            System.out.println(Thread.currentThread().getName()+"-->"+ticketNum--);
+        }
+    }
+    public static  void main (String []args){
+        //一份资源
+        BuildThread webT=new BuildThread();
+        //多个代理
+        new Thread(webT,"xiaohong").start();
+        new Thread(webT,"xiaohuang").start();
+        new Thread(webT,"xiaolv").start();
+    }
+}*/
+//抢票   线程调用start（）方法并不意味着立即执行  而是到就绪状态等待cpu调度，所以每次执行的结果都是不一样的
+
+import java.util.concurrent.*;
+
+//实现callable  重现call方法  call有返回值且可以抛出异常
+public class BuildThread implements Callable<Integer>{
+    private int step = 0;
+    private String name;
+    private long time;
+    private boolean flag = true;
+    BuildThread(String name,long time){
+        this.name=name;
+        this.time=time;
+    }
+    @Override
+    public Integer call() throws Exception {
+        while(flag){
+            Thread.sleep(time);
+            step++;
+        }
+        return step;
+    }
+    public void setFlag(boolean flag){
+        this.flag = flag;
+    }
+    public static void main (String [] args) throws InterruptedException, ExecutionException {
+        //创建callable的实现类对象
+        BuildThread tortoise = new BuildThread("tortoise",0);
+        BuildThread rabit = new BuildThread("rabit",100);
+        //创建执行服务
+        ExecutorService ser = Executors.newFixedThreadPool(2);
+        //提交执行，得到future对象
+        Future<Integer> resultR=ser.submit(tortoise);
+        Future<Integer> resultT=ser.submit(rabit);
+
+        Thread.sleep(3000);
+        rabit.setFlag(false);
+        tortoise.setFlag(false);
+
+        //获取结果
+        int numR = resultR.get();
+        int numT = resultT.get();
+
+        System.out.println("rabbit"+numR);
+        System.out.println("tortoise"+numT);
+
+        //停止服务
+        ser.shutdown();
+    }
+}
+/**
+ * 1、创建一个类实现Callable接口，然后重写call()方法
+ * （和run方法不一样的是，call方法可以有返回值，并且可以抛出异常）
+ * 2、创建Callable的实现类对象--》创建执行服务
+ * --》提交执行服务得到Future对象--》获取结果--》停止服务
+ */
+
+
---
+++ src/com/acat/lijingwen/wednesday/offer.java
@@ -0,0 +1,73 @@
+package com.acat.lijingwen.wednesday;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class offer {
+//    public static ArrayList<Integer> huiwenshu(int length, int []list){
+//
+//    }
+//    public static void input(){
+//        int [] list = new int[1000];
+//        int length;
+//        Scanner input=new Scanner(System.in);
+//        System.out.print("请您输入数组的大小:");
+//        length=input.nextInt(); //输入整型
+//        for(int i=0;i<length;i++){
+//            System.out.println("输入最后第"+i+"个值，共"+length);
+//            list[i]=input.nextInt(); //输入字符串型
+//
+//        }
+//    }
+//    public static void main (String []args){
+//        input();
+//    }
+//}
+
+
+/**
+ * Dynamic Programming
+ *
+ * State:
+ *   dp[i][j]: 表示a[i],...,a[j]中的回文子序列的最大和
+ *
+ * Initial State:
+ *   dp[i][i] = a[i]
+ *
+ * State Transition:
+ *   if (a[i] == a[j]) dp[i][j] = dp[i + 1][j - 1] + 2 * a[i];
+ *   else dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+ *
+ * @author wylu
+ */
+//20018搜狐笔试  回文数组
+///***********************************************************************没有理解************************************************
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] strs = br.readLine().split(" ");
+        int[] a = new int[n];
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(strs[i]);
+            sum += a[i];
+        }
+
+        long[][] dp = new long[n][n];
+        for (int i = a.length - 1; i >= 0; i--) {
+            dp[i][i] = a[i];
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] == a[j])
+                    dp[i][j] = dp[i + 1][j - 1] + 2 * a[i];
+                else
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+        System.out.println(2 * sum - dp[0][n - 1]);
+    }
+}
+
