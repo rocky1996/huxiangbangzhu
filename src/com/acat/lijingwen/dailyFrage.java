5a934c94bcdd989741312730686a62481f490ad3
LJWhappy <ljw2797661571@163.com>
2019-10-29 22:28:30 +0800

    java基础  redis菜鸟教程

--- src/com/acat/lijingwen/dailyFrage.java
+++ src/com/acat/lijingwen/dailyFrage.java
@@ -1,7 +1,10 @@
 package com.acat.lijingwen;
 
+import javax.annotation.processing.SupportedAnnotationTypes;
+import java.util.Scanner;
+
 public class dailyFrage {
-    //引用数据类型
+  /*  //引用数据类型
     public static void main(String [] args){
         int a=10,b=20;
         change(a,b);
@@ -27,8 +30,8 @@ public class dailyFrage {
     public static void change(int x,int y){
         x=100;
         y=200;
-    }
-}
+    }*/
+
 /*
 解析：此处为栈内存和堆内存的关系
 栈内存的话会自动申请和释放内存，方法中的变量生存时间就是方法运行，
@@ -42,3 +45,72 @@ V1=ni,v2=da
 <3>
 2  4  6  8  10
 */
+
+/********************2019.10.29-
+/**java基础规范
+ * 类的第一个字母大写
+ * */
+/*
+* 设计出物品这种类
+类名：Item
+物品有如下属性:
+名字 name 类型是字符串String
+价格 price 类型是整型 int
+
+创建(实例化)3件具体物品
+名称 价格
+血瓶 50
+草鞋 300
+长剑 350
+* */
+/*   String name;
+   int price;
+    public static  void main(String [] args){
+        dailyFrage df=new dailyFrage();
+        df.name="血瓶";
+        df.price=12;
+        dailyFrage df1 = new dailyFrage();
+        df1.name="";
+        df1.price=15;
+        dailyFrage df2 =new dailyFrage();
+        df2.name="";
+        df2.price=56;
+    }*/
+
+/*java 输入  */
+/*import java.util.Scanner;
+Scanner sc = new Scanner(System.in);
+sc.nextInt() //输入整数
+    sc.nextFloat(); //输入浮点数
+    sc.nextLine();//输入字符串*/
+
+/*
+类的转化：
+    子类一定可以转父类 ，但父类不一定可以转子类要转就得强制转化  ,没有继承关系的两个类互相转化肯定失败
+    转化不行的时候：
+    Hero h = new Hero();
+    ADHero ad=new ADHero();
+    Support s =new Support();
+    h=s;  是子类转父类，是可以转换成功的
+    ad=(ADHero)h;  是把h引用所指向的对象 Support，转换为ad引用的类型ADHero。 从语义上讲，把物理攻击英雄，当成辅助英雄来用，说不通，所以会强制转换失败，并且抛出异常
+*/
+
+/*重写
+* 子类可以继承父类的对象方法
+在继承后，重复提供该方法，就叫做方法的重写
+又叫覆盖 override
+* 多态
+* 操作符的多态
++ 可以作为算数运算，也可以作为字符串连接
+类的多态
+父类引用指向子类对象
+* 多态: 都是同一个类型，调用同一个方法，却能呈现不同的状态
+*要实现类的多态，需要如下条件
+1. 父类（接口）引用指向子类对象
+2. 调用的方法有重写
+* */
+
+
+
+
+}
---
+++ src/com/acat/lijingwen/friday/connectService.java
@@ -0,0 +1,51 @@
+package com.acat.lijingwen.friday;
+import redis.clients.jedis.Jedis;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+/*
+*redis的配置和导包
+* redisService
+* 导入jar包做法  自行下载jedis-2.9.0.jar  commons-pool2-2.4.3.jar包
+* file->project strucu..->modules->add->选择导入jar的包
+* */
+public class connectService {
+    //连接到redis服务
+  /*  public static void main(String[]args){
+        //连接本地的redis服务
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("链接成功");
+        //查看服务是否运行
+        System.out.println("服务正在运行："+jedis.ping());
+    }*/
+
+ /*   public static void main(String[]args){
+        //连接本地的 Redis 服务
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("连接成功");
+        //存储数据到列表中
+        jedis.lpush("site-list", "Runoob");
+        jedis.lpush("site-list", "Google");
+        jedis.lpush("site-list", "Taobao");
+        // 获取存储的数据并输出
+        List<String> list = jedis.lrange("site-list", 0 ,2);
+        for(int i=0; i<list.size(); i++) {
+            System.out.println("列表项为: "+list.get(i));
+        }
+    }*/
+    //redis java keys实例
+/*    public static void main(String [] args) {
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("连接成功");
+        //获取数据并输出
+        Set<String> keys = jedis.keys("*");
+        Iterator<String> it=keys.iterator();
+        while(it.hasNext()){
+            String key = it.next();
+            System.out.println(key);
+        }
+    }*/
+
+
+}
