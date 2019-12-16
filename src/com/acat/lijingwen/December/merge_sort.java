32c803db39aaf79b1767ed2abb6fb4e2d430a227
LJWhappy <2797661571@qq.com>
2019-12-16 22:03:41 +0800

    2019.12.11-2019.12.16

---
+++ src/com/acat/lijingwen/December/merge_sort.java
@@ -0,0 +1,35 @@
+//package com.acat.lijingwen.December;
+//
+//public class merge_sort {
+//    //归并排序是  采用分治法  将有序的子序列合并 得到完全有序的序列；即先使每个子序列有序，再使
+//    // 子序列段间有序。若将两个有序表合并成一个有序表  成为二路归并。
+//    /**
+//     * 两个子序列是排好序的
+//     * */
+//    public static void main(String args[]){
+//        int [] arr1={2,7,8};
+//        int [] arr2={1,4,9};
+//        int [] arr = new int [arr1.length+arr2.length];
+//        merge_sort(arr1,arr2,arr);
+//
+//    }
+//    public static void merge_sort(int arr1[],int arr2[],int arr[] ){
+//
+//        int pos1,pos2;
+//        if (arr1.length>arr2.length){
+//            pos1 = arr1.length;
+//            pos2 = arr2.length;
+//        }else
+//            pos1 = arr2.length;
+//            pos2 = arr1.length;
+//
+//        for(int i=0;i<pos1;i++) {
+//            for (int j = 0; j < pos1; j++) {
+//                if[arr1=]
+//            }
+//        }
+//        for (int i=0;i<arr.length;i++){
+//            System    .out.println(arr[i]);
+//        }
+//    }
+//}
